367cae13f8b278234815bb4085e63eb5cde25537
yousha-waja <youshawaja01@gmail.com>
2023-08-24 22:15:02 +0200

    fixed phonebill function

--- bootcamp/totalPhoneBill.js
+++ bootcamp/totalPhoneBill.js
@@ -1,23 +1,15 @@
-export default function totalPhoneBill(string) {
-   let data = string.toLowerCase().split(',');
-   let total = 0;
-   console.log(data);
-   let isValid = true;
-
-   for (let i = 0; i < data.length; i++) {
-       if (data[i] === 'call') {
-           total += 2.75;
-       } else if (data[i] === 'sms') {
-           total += 0.65;
-       } else {
-           isValid = false;
-           break;
+export default function totalPhoneBill(string){
+    let data = string.toLowerCase().split(',');
+    let total=0;
+     console.log(data);
+     for(let i=0;i<data.length;i++){
+      if(data[i]=== 'call'){
+         total += 2.75;
+      }else if(data[i] === 'sms'){
+         total += 0.65;
        }
+       else {
+        return "Invalid input: Only 'call' and 'sms' are allowed separated by a comma.";
+    }
+     } return (`Your total phone bill is R${total.toFixed(2)}`);
    }
-
-   if (isValid) {
-       return `Your total phone bill R${total.toFixed(2)}`;
-   } else {
-       return "Invalid input: Only 'call' and 'sms' are allowed separated by a comma.";
-   }
-}
